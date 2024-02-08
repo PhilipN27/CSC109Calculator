9fec7ec5d4882ead3316653a5424b8c5a050aa11
Philip <panora@quinnipiac.edu>
2024-02-07 19:12:48 -0500

    Step 1

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Calculator
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello");
+        int num1, num2, sum;
+        //Create a Scanner object
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first number: ");
+        num1 = scanner.nextInt(); //read user input 1
+
+        System.out.println("Enter second number: ");
+        num2 = scanner.nextInt(); //read user input 2
+
+        sum = num1 + num2; //calculate sum of number 1 and 2
+
+        System.out.println("The sum is: " + sum); //Prints sum
     }
 }
